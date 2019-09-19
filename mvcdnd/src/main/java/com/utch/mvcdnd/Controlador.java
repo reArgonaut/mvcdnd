8c48c4c4f8ac0d15d7e46483f73341ae56b0d536
Franguestclain <the_king_00carlos@hotmail.com>
2019-09-19 16:10:19 -0600

    Ya hace todo, menos el historial y otras cosas

--- mvcdnd/src/main/java/com/utch/mvcdnd/Controlador.java
+++ mvcdnd/src/main/java/com/utch/mvcdnd/Controlador.java
@@ -28,24 +28,42 @@ public class Controlador {
         do{
             opcion = leerEntero();
             switch(opcion){
+                // d4
                 case 1:
-                    
+                    int res = repetirOp(v.cDados(), 4, m);
+                    m.historial(res);
+                    v.resDado(res);
                     break;
+                // d6
                 case 2:
+                    v.resDado(repetirOp(v.cDados(), 6, m));
                     break;
+                // d8
                 case 3:
+                    v.resDado(repetirOp(v.cDados(), 8, m));
                     break;
+                // d10
                 case 4:
+                    v.resDado(repetirOp(v.cDados(), 10, m));
                     break;
+                // d12
                 case 5:
+                    v.resDado(repetirOp(v.cDados(), 12, m));
                     break;
+                // d100
                 case 6:
+                    v.resDado(repetirOp(v.cDados(), 100, m));
                     break;
+                // d20
                 case 7:
+                    v.resDado(repetirOp(v.cDados(), 20, m));
                     break;
+                // Varios tipos
                 case 8:
+                    v.menu();
                     break;
-                default:
+                case 9:
+                    v.historial();
                     break;
             }
         }while(opcion!=9);
@@ -61,6 +79,12 @@ public class Controlador {
             e.printStackTrace();
         }
         return n;
+    }
+    
+    public int repetirOp(int repes, int tipo, Modelo m){
+        m.setType(tipo);
+        m.setdice(repes);
+        return m.getRoll();
     }
     
     

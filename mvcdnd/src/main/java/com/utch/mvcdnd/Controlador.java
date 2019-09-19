8ff033bfea9659ed41f7212c24125d7c4a7e9f90
Franguestclain <the_king_00carlos@hotmail.com>
2019-09-19 13:09:05 -0600

    Añadido Controlador

---
+++ mvcdnd/src/main/java/com/utch/mvcdnd/Controlador.java
@@ -0,0 +1,67 @@
+package com.utch.mvcdnd;
+
+import Vista;
+import Modelo;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author carlos
+ */
+public class Controlador {
+    private Vista v;
+    private Modelo m;
+    
+    
+    public Controlador(Vista v, Modelo m){
+        this.v = v;
+        this.m = m;
+        init();
+    }
+    
+    public void init(){
+        v.menu();
+        
+        int opcion;
+        
+        do{
+            opcion = leerEntero();
+            switch(opcion){
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                default:
+                    break;
+            }
+        }while(opcion!=9);
+        
+    }
+    
+    public int leerEntero(){
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        int n=0;        
+        try{
+            n = Integer.parseInt(r.readLine());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return n;
+    }
+    
+    
+}

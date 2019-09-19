07e89d4e7cbe964c091c0a5f9cce014d266ce676
Adrian <Adrian@reDragon>
2019-09-19 13:16:23 -0600

    anadi la vista y corregi controlador

--- mvcdnd/src/main/java/com/utch/mvcdnd/Main.java
+++ mvcdnd/src/main/java/com/utch/mvcdnd/Main.java
@@ -11,6 +11,6 @@ package com.utch.mvcdnd;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");       
     }
 }
